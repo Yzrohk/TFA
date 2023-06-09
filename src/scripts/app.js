4db2a2253ef17d22a9badbef29c37e9ad63b1bf5
Yzrohk <mdlol2011@hotmail.fr>
2023-06-09 10:15:36 +0200

    Finalisation pages principales + creation credits.html, ui-kit.html et video.html

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,15 @@
 "use strict";
+
+document.addEventListener('scroll', function() {
+    var sections = document.querySelectorAll('.section');
+    var windowHeight = window.innerHeight;
+    var scrollTop = window.pageYOffset;
+  
+    sections.forEach(function(section) {
+      var offsetTop = section.offsetTop;
+  
+      if (scrollTop + windowHeight > offsetTop) {
+        section.classList.add('visible');
+      }
+    });
+  });
