63ddce5b807789de53781ca31b0cb4ebbdc37997
Yzrohk <mdlol2011@hotmail.fr>
2023-08-18 18:33:48 +0200

    peaufinement des détails, ajouts animations + GSAP, fix images et textes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,21 +1,47 @@
 "use strict";
+gsap.registerPlugin(ScrollTrigger);
 
-/*function scrollAffichage() {
-document.addEventListener('scroll', function () {
-  var sections = document.querySelectorAll('.section');
-  var windowHeight = window.innerHeight;
-  var scrollTop = window.pageYOffset;
+    const titres = gsap.utils.toArray('.titre--h2');
+    const paragraphes = gsap.utils.toArray('.texte');
+    const images = gsap.utils.toArray(".image--text")
 
-  sections.forEach(function (section) {
-    var offsetTop = section.offsetTop;
+    // Animation pour les titres
+    titres.forEach(titre => {
+      gsap.from(titre, {
+        scrollTrigger: {
+          trigger: titre,
+          start: 'top 85%', // Fait apparaître le titre quand il est à 80% en dessous de la vue
+        },
+        opacity: 0,
+        y: 50,
+        duration: 1
+      });
+    });
 
-    if (scrollTop + windowHeight > offsetTop) {
-      section.classList.add('visible');
-    }
-  });
-});
-}
-scrollAffichage();*/
+    images.forEach(titre => {
+      gsap.from(titre, {
+        scrollTrigger: {
+          trigger: titre,
+          start: 'top 85%', // Fait apparaître le titre quand il est à 80% en dessous de la vue
+        },
+        opacity: 0,
+        y: 50,
+        duration: 1
+      });
+    });
+
+    // Animation pour les paragraphes
+    paragraphes.forEach(paragraphe => {
+      gsap.from(paragraphe, {
+        scrollTrigger: {
+          trigger: paragraphe,
+          start: 'top 85%', // Fait apparaître le paragraphe quand il est à 80% en dessous de la vue
+        },
+        opacity: 0,
+        y: 50,
+        duration: 1
+      });
+    });
 
 const links = document.querySelectorAll('.el__lien');
 
