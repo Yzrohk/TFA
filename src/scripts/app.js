1d6742961f1cbd394e0bc5b77431523b720f527c
Yzrohk <mdlol2011@hotmail.fr>
2023-08-18 19:19:41 +0200

    fix menu, header + footerin credits.html ui-kit.html et video.html + maj composants credits.html et uikit.html

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,14 +3,25 @@ gsap.registerPlugin(ScrollTrigger);
 
     const titres = gsap.utils.toArray('.titre--h2');
     const paragraphes = gsap.utils.toArray('.texte');
-    const images = gsap.utils.toArray(".image--text")
+    const images = gsap.utils.toArray(".image--text");
 
-    // Animation pour les titres
+    images.forEach(image => {
+      gsap.from(image, {
+        scrollTrigger: {
+          trigger: image,
+          start: 'top 85%', 
+        },
+        opacity: 0,
+        y: 50,
+        duration: 1
+      });
+    });
+
     titres.forEach(titre => {
       gsap.from(titre, {
         scrollTrigger: {
           trigger: titre,
-          start: 'top 85%', // Fait apparaître le titre quand il est à 80% en dessous de la vue
+          start: 'top 85%',
         },
         opacity: 0,
         y: 50,
@@ -18,24 +29,11 @@ gsap.registerPlugin(ScrollTrigger);
       });
     });
 
-    images.forEach(titre => {
-      gsap.from(titre, {
-        scrollTrigger: {
-          trigger: titre,
-          start: 'top 85%', // Fait apparaître le titre quand il est à 80% en dessous de la vue
-        },
-        opacity: 0,
-        y: 50,
-        duration: 1
-      });
-    });
-
-    // Animation pour les paragraphes
     paragraphes.forEach(paragraphe => {
       gsap.from(paragraphe, {
         scrollTrigger: {
           trigger: paragraphe,
-          start: 'top 85%', // Fait apparaître le paragraphe quand il est à 80% en dessous de la vue
+          start: 'top 85%',
         },
         opacity: 0,
         y: 50,
