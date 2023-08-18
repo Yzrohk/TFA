5fb517f8d1ac3afa235cc3f9711ccd086a0464cd
Yzrohk <mdlol2011@hotmail.fr>
2023-08-18 13:16:06 +0200

    réorganisation totales, ajouts séparation en plusieurs components SCSS, fix burger menu, navigation entièrément retravaillée, suppression motautop.html, suppression animation blocs, fix problèmes UX (soulignements, etc), changement de certaines typos, fix balise picture, tentative fix imbrication SASS

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function scrollAffichage() {
+/*function scrollAffichage() {
 document.addEventListener('scroll', function () {
   var sections = document.querySelectorAll('.section');
   var windowHeight = window.innerHeight;
@@ -15,31 +15,54 @@ document.addEventListener('scroll', function () {
   });
 });
 }
-scrollAffichage();
+scrollAffichage();*/
 
+const links = document.querySelectorAll('.el__lien');
+
+  links.forEach(link => {
+    link.addEventListener('click', function(event) {
+
+      // Supprimer la classe 'active' du lien précédemment cliqué
+      const previouslyActive = document.querySelector('.el__lien.lastClicked');
+      if (previouslyActive) {
+        previouslyActive.classList.remove('lastClicked');
+      }
+
+      // Ajouter la classe 'active' au lien cliqué
+      this.classList.add('lastClicked');
+      slideBurgerMenu();
+    });
+  });
+
+
+var btnBurgermenu = document.querySelector(".menu__bouton");
+var listeSvgBurgermenu = document.querySelectorAll(".svg__burgermenu");
+var menuListe = document.querySelector(".menu__liens");
 function burgerMenu () {
-  var btnBurgermenu = document.querySelector(".menu__bouton");
-  var listeSvgBurgermenu = document.querySelectorAll(".svg__burgermenu");
-  var menuListe = document.querySelector(".menu__liens");
 
   if (btnBurgermenu && listeSvgBurgermenu && menuListe) {
-    btnBurgermenu.addEventListener("click", activateBurgermenu);
-
-    function activateBurgermenu() {
-        listeSvgBurgermenu.forEach(svg => {
-            if(svg.classList.contains("svg--active")) {
-                svg.classList.toggle("svg--inactive");
-                svg.classList.toggle("svg--active");
-            }
-            else if (svg.classList.contains("svg--inactive")) {
-                svg.classList.toggle("svg--active");
-                svg.classList.toggle("svg--inactive");
-            }
-        });
-
-        menuListe.classList.toggle("menu__liens--active")
-
-    }
+    btnBurgermenu.addEventListener("click", slideBurgerMenu);
   }
 }
 burgerMenu();
+
+// Change la classe 'active' sur le Burger Menu
+function toggleMenu() {
+  menuListe.classList.toggle("menu__liens--active");
+}
+
+// Slide le burger menu
+function slideBurgerMenu() {
+  listeSvgBurgermenu.forEach(svg => {
+      if(svg.classList.contains("svg--active")) {
+          svg.classList.toggle("svg--inactive");
+          svg.classList.toggle("svg--active");
+      }
+      else if (svg.classList.contains("svg--inactive")) {
+          svg.classList.toggle("svg--active");
+          svg.classList.toggle("svg--inactive");
+      }
+  });
+  toggleMenu();
+}
+
